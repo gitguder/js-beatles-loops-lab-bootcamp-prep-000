be23fca459c25c7f3269670840b6f68062462a34
Codey Pinero <codey.pinero@gmail.com>
2019-02-16 06:30:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]}  plays instruments[i]`;
+    }
+    return array;
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love The Beatles!");
+    number ++;
+  } while (number < 15);
+return array;
+};
